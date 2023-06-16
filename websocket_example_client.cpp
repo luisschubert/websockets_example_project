1d0ad114138c09605d3de5cd09ec81dac0655904
Luis-Johannes Schubert <lschubert@tesla.com>
2023-06-16 15:06:56 -0700

    loop[json.exception.type_error.305] cannot use operator[] with a string argument with array is the error here.

--- websocket_example_client.cpp
+++ websocket_example_client.cpp
@@ -1,8 +1,10 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
+
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/strand.hpp>
+
 #include <chrono>
 #include <iostream>
 #include <thread>
@@ -17,8 +19,8 @@ int main() {
     boost::asio::io_context ioc;
     tcp::resolver resolver(ioc);
 
-    std::cout << "[Client] resolve with localhost 8080\n";
-    auto const results = resolver.resolve("localhost", "8080");
+    std::cout << "[Client] auto const results = resolver.resolve(\"localhost\", \"8080\");\n";
+    auto const results = resolver.resolve("server", "8080");
     websocket_stream ws(ioc);
     
     bool connected = false;
@@ -29,10 +31,11 @@ int main() {
             std::cout << "[Client] connect with localhost\n";
             auto ep = boost::asio::connect(ws.next_layer(), results);
             std::cout << "[Client] Handshake with localhost\n";
-            ws.handshake("localhost", "/");
-            // ws.handshake("server", "/");
+            // ws.handshake("localhost", "/");
+            ws.handshake("server", "/");
             connected = true;
         } catch (const std::exception& e) {
+            std::cout << "[Client]" << e.what() <<"\n";
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     }
@@ -48,20 +51,38 @@ int main() {
     ws.read(buffer);
     std::cout << "[Client] Got Heartbeat Period Response\n";
 
-    auto period = json::parse(boost::beast::buffers_to_string(buffer.data()))["period"];
+    try {
+        auto buffer_data = buffer.data();
+        std::size_t total_size = 0;
+        for (const auto& buffer_entry : buffer_data)
+        {
+            total_size += buffer_entry.size();
+        }
+        std::cout << "buffer_data: " << total_size << " bytes" << std::endl;
+        // std::cout << "buffer_data: " << buffer_data.size() << " bytes" << std::endl;
+        auto buffer_as_string = boost::beast::buffers_to_string(buffer_data);
+        std::cout << "buffer_as_string: " << buffer_as_string << std::endl;
+        auto buffer_parsed = json::parse(buffer_as_string);
+        std::cout << "buffer_parsed (type: " << typeid(buffer_parsed).name() << "): " << buffer_parsed << std::endl;
+        auto period = buffer_parsed["period"];
+        for (;;) {
+            auto period_in_seconds = std::chrono::seconds(period);
 
-    for (;;) {
-        auto period_in_seconds = std::chrono::seconds(period);
+            std::cout << "[Client] Sleeping for "  << period << "\n";
+            std::this_thread::sleep_for(period_in_seconds);
+            std::cout << "[Client] Done Sleeping\n";
 
-        std::cout << "[Client] Sleeping for "  << period << "\n";
-        std::this_thread::sleep_for(period_in_seconds);
-        std::cout << "[Client] Done Sleeping\n";
+            std::cout << "[Client] Writing Heartbeat Request \n";
+            ws.write(boost::asio::buffer(json{{"client_id", "client_number_1"}}.dump()));
+            std::cout << "[Client] Wrote Heartbeat Request\n";
 
-        std::cout << "[Client] Writing Heartbeat Request \n";
-        ws.write(boost::asio::buffer(json{{"client_id", "client_number_1"}}.dump()));
-        std::cout << "[Client] Wrote Heartbeat Request\n";
-
-        ws.read(buffer);
-        std::cout << "[Client] Got Heartbeat Response\n";
+            ws.read(buffer);
+            std::cout << "[Client] Got Heartbeat Response\n";
+        }
+    } catch (const std::exception& e) {
+            std::cout << "[Client] loop" << e.what() <<"\n";
+            std::this_thread::sleep_for(std::chrono::seconds(1));
     }
+
+
 }
--- websocket_example_server.cpp
+++ websocket_example_server.cpp
@@ -1,8 +1,10 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
+
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/strand.hpp>
 #include <boost/asio/io_context.hpp>
+
 #include <thread>
 #include <iostream>
 #include "json.hpp"
@@ -14,25 +16,40 @@ using websocket_stream = websocket::stream<tcp::socket>;
 
 int main() {
     boost::asio::io_context ioc;
+    std::cout << "[Server] tcp::acceptor acceptor(ioc, {tcp::v4(), 8080});\n";
     tcp::acceptor acceptor(ioc, {tcp::v4(), 8080});
 
     for (;;) {
+        std::cout << "[Server] tcp::socket socket(ioc);\n";
         tcp::socket socket(ioc);
+
+        std::cout << "[Server] acceptor.accept(socket)\n";
         acceptor.accept(socket);
+
+        std::cout << "[Server] std::thread([socket = std::move(socket)]()\n";
         std::thread([socket = std::move(socket)]() mutable {
+
+            std::cout << "[Server] websocket_stream ws(std::move(socket));\n";
             websocket_stream ws(std::move(socket));
+
+            std::cout << "[Server] ws.accept();\n";
             ws.accept();
 
             for (;;) {
                 boost::beast::multi_buffer buffer;
+                std::cout << "[Server] ws.read(buffer);\n";
                 ws.read(buffer);
+
+                std::cout << "[Server] auto msg = json::parse(boost::beast::buffers_to_string(buffer.data()));\n";
 
                 auto msg = json::parse(boost::beast::buffers_to_string(buffer.data()));
                 if (msg.empty()) {
                     // HeartbeatPeriodRequest
+                    std::cout << "[Server] write HeartbeatPeriodRequest\n";
                     ws.write(boost::asio::buffer(json{{"period", 100}}.dump()));
                 } else {
                     // HeartbeatRequest
+                    std::cout << "[Server] write HeartbeatRequest\n";
                     ws.write(boost::asio::buffer(json{{}}.dump()));
                 }
             }
